0c1ce26af61c8600137ead36c38e81be64c922c5
Axel <axelsparr@hotmail.com>
2022-02-28 17:16:36 +0100

    fixa typeo i inside_mesh.py

--- scripts/neural_parts/external/libmesh/inside_mesh.py
+++ scripts/neural_parts/external/libmesh/inside_mesh.py
@@ -1,5 +1,5 @@
 import numpy as np
-from .triangle_hash import TriangleHash as _TriangleHash
+from triangle_hash import TriangleHash as _TriangleHash
 
 
 def check_mesh_contains(mesh, points, hash_resolution=512):
